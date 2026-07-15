ee7d363b55356ed834af3d412ce37845ac187180
gad-biton <gadb633@gmail.com>
2026-07-15 14:59:00 +0300

    added 4. Private Follower Count

--- basic_exercise.py
+++ basic_exercise.py
@@ -24,21 +24,41 @@
 # print(profile.username)
 # print(profile.email)  
 
-# 3. Username Setter with Validation
+# # 3. Username Setter with Validation
+# class UserProfile:
+#     def __init__(self,username):
+#         self.__username=username
+#     @property
+#     def username(self):
+#         return self.__username
+#     @username.setter
+#     def username(self,new_username):
+#         self.__username =new_username if len(new_username) >=3 else print("Username too short")
+
+
+# p = UserProfile("alice")
+# p.username = "ab"    
+# p.username = "alexis"
+# print(p.username)
+
+# 4. Private Follower Count
 class UserProfile:
     def __init__(self,username):
         self.__username=username
+        self.__followers = 0
     @property
-    def username(self):
-        return self.__username
-    @username.setter
-    def username(self,new_username):
-        self.__username =new_username if len(new_username) >=3 else print("Username too short")
+    def followers(self):
+        return self.__followers
+    def follow(self):
+        self.__followers +=1
+    def unfollow(self):
+        self.__followers -=1 if self.__followers > 0 else None
+profile = UserProfile("gad")
+profile.follow()
+profile.follow()                    
+profile.follow()                    
+profile.unfollow()
+print(profile.followers)
 
-
-p = UserProfile("alice")
-p.username = "ab"    
-p.username = "alexis"
-print(p.username)
 
 
