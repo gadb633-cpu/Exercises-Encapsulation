b091790034e74cc32a17e8f83ef33727a77464cb
gad-biton <gadb633@gmail.com>
2026-07-15 14:35:49 +0300

    added 2. Private Email with Getter

--- basic_exercise.py
+++ basic_exercise.py
@@ -1,10 +1,26 @@
-# 1. Private Username
+# # 1. Private Username
+# class UserProfile:
+#     def __init__(self,username):
+#         self.__username=username
+#     @property
+#     def username(self):
+#         return self.__username
+# profile = UserProfile("alice99")
+# print(profile.username )      
+# print(profile.__username )
+
+# 2. Private Email with Getter
 class UserProfile:
-    def __init__(self,username):
-        self.__username=username
+    def __init__(self,username, email):
+        self.__username = username
+        self.__email = email
     @property
     def username(self):
         return self.__username
-profile = UserProfile("alice99")
-print(profile.username )      
-print(profile.__username )      
+    @property
+    def email(self):
+        return self.__email
+profile = UserProfile("bob", "bob@mail.com")
+print(profile.username)
+print(profile.email)    
+
